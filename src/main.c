dd6dc1429877d3df0856ae6668ac38e802df04b6
Filipe Ferreira <filipejosexavierferreira@gmail.com>
2019-03-11 16:48:20 +0000

    Added some changes for testing purposes

--- src/main.c
+++ src/main.c
@@ -1,15 +1,18 @@
 #include "parser.h"
+#include "dirtree.h"
 #include <stdio.h>
 
 #define NO_FLAGS 3
 
 int main(int argc, char *argv[]) {
-    char *flagArguments[NO_FLAGS];
-    for(size_t i = 0; i < NO_FLAGS; i++)
-    {
-        flagArguments[i] = "";
-    }
+    // char *flagArguments[NO_FLAGS];
+    // for(size_t i = 0; i < NO_FLAGS; i++)
+    // {
+    //     flagArguments[i] = "";
+    // }
     
-    parseArguments(argc, argv, flagArguments, NO_FLAGS);
+    // parseArguments(argc, argv, flagArguments, NO_FLAGS);
+    printf("%s\n",argv[1]); 
+    listdtree(argv[1],1);
     return 0;
 }
