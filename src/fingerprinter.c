94452ef3c9f00dcfd868f9af5a611cf35e3f9d0b
Carlos Eduardo <cnova1998@gmail.com>
2019-03-12 14:44:04 +0000

    Exec now searches for md5sum command in user path

--- src/fingerprinter.c
+++ src/fingerprinter.c
@@ -8,12 +8,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+char* cenas()
+{
+    char *teste = malloc(sizeof(char)*1024);
+    return teste;
+}
+
 int md5(char *name, FILE* file)
 {
     /**
-     *  TODO: Separate md5()'s fork code into two functions: one for the path finding, and another for executing md5sum
-     *  The first function should give a char* with the path to md5sum. Need to find a way to make this work
-     *  (without bad access errors).
+     * TODO: Handle possible errors with the code
      */
 
     pid_t pid;
@@ -21,12 +26,6 @@ int md5(char *name, FILE* file)
     char md5sumPath[1024];
     int fd[2];
 
-    if (pipe(fd) == -1)
-    {
-        perror("Error creating pipe: ");
-        exit(1);
-    }
-
     pid = fork();
     if (pid == -1)
     {
@@ -38,39 +37,7 @@ int md5(char *name, FILE* file)
         close(fd[0]);
         dup2(fd[1], STDOUT_FILENO);
         close(fd[1]);
-        execl("/usr/bin/which", "which", "md5sum", NULL);
-        perror("Failed to execute which: ");
-        exit(3);
-    }
-    else
-    {
-        int stat;
-        wait(&stat);
-        close(fd[1]);
-        while (read(fd[0], md5sumPath, sizeof(md5sumPath)));
-        close(fd[0]);
-    }
-
-    md5sumPath[strlen(md5sumPath)-1] = '\0';
-
-    if (pipe(fd) == -1)
-    {
-        perror("Error creating pipe: ");
-        exit(1);
-    }
-
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("Error creating fork: ");
-        exit(2);
-    }
-    else if (pid == 0)
-    {
-        close(fd[0]);
-        dup2(fd[1], STDOUT_FILENO);
-        close(fd[1]);
-        execl(md5sumPath, "md5sum", name, NULL);
+        execlp("md5sum", "md5sum", name, NULL);
         perror("Failed to execute md5sum: ");
         exit(3);
     }
