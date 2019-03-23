587d10e261af317340108ecd8bd09227299a33ed
Gustavo Torres <torresgustam@gmail.com>
2019-03-23 15:09:42 +0000

    New implementation of log

--- src/log.h
+++ src/log.h
@@ -6,6 +6,12 @@
 int open_file(FILE** file, const char *pathname, const char *mode);
 int close_file(FILE* file);
 double get_time();
-void log_activity(FILE* file, double initialTime, char* eventDescription);
+void log_activity(char* eventDescription);
+
+void set_file(FILE* file);
+FILE* get_file();
+
+void set_initialTime(double time);
+double get_initialTime();
 
 #endif
